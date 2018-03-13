49993855dece9550d95a4a1add5c5b16cab22e24
Weston Hancock <weston.hancock@liferay.com>
2018-03-13 13:12:37 -0700

    minor ui changes

--- components/Message.js
+++ components/Message.js
@@ -6,13 +6,8 @@ export default class Message extends Component {
 		const vars = this.props.vars;
 
 		return (
-			<p>
-				Your retirement savings would be ${person.nestEgg.toFixed(2)}.
-				It would last {person.yrsRetired} years at ${vars.retirementIncome} per year if you started
-				work at age 25 and retired at {25 + vars.workingYears} with a starting salery of
-				${person.initialIncome} and a ${vars.raisePercent}% raise annually while saving
-				{vars.savingPercent * 100}% of your net income each year that accures interest at
-				{vars.curInterestRate * 100}%. Better not live past {person.yrsRetired + vars.workingYears + 25}.
+			<p className="results-message">
+				Your retirement savings would be ${person.nestEgg.toFixed(2)}. It would last {person.yrsRetired} years at ${vars.retirementIncome} per year if you started work at age 25 and retired at {25 + vars.workingYears} with a starting salery of ${person.initialIncome} and a {vars.raisePercent}% raise annually while saving {vars.savingPercent * 100}% of your net income each year that accures interest at {vars.curInterestRate * 100}%. Better not live past {person.yrsRetired + vars.workingYears + 25}.
 			</p>
 		);
 	}
