1292178fd16a1a348ad10c929fe1b65fa11f098c
Weston Hancock <weston.hancock@liferay.com>
2018-03-13 16:55:28 -0700

    use state fully

--- components/Message.js
+++ components/Message.js
@@ -2,12 +2,9 @@ import React, {Component} from 'react';
 
 export default class Message extends Component {
 	render() {
-		const person = this.props.person;
-		const vars = this.props.vars;
-
 		return (
 			<p className="results-message">
-				Your retirement savings would be ${person.nestEgg.toFixed(2)}. It would last {person.yrsRetired} years at ${vars.retirementIncome} per year if you started work at age 25 and retired at {25 + vars.workingYears} with a starting salery of ${person.initialIncome} and a {vars.raisePercent}% raise annually while saving {vars.savingPercent * 100}% of your net income each year that accures interest at {vars.curInterestRate * 100}%. Better not live past {person.yrsRetired + vars.workingYears + 25}.
+				Your retirement savings would be ${this.props.nestEgg}. It would last {this.props.yrsRetired} years at ${this.props.retirementIncome} per year if you started work at age 25 and retired at {25 + this.props.workingYears} with a starting salery of ${this.props.initialIncome} and a {this.props.raisePercent}% raise annually while saving {this.props.savingPercent * 100}% of your net income each year that accures interest at {this.props.curInterestRate * 100}%. Better not live past {this.props.yrsRetired + this.props.workingYears + 25}.
 			</p>
 		);
 	}
--- ui/main.js
+++ ui/main.js
@@ -8,82 +8,76 @@ import Results from '../components/Results';
 import ResultsData from '../components/ResultsData';
 
 let form;
-let person = {};
-let vars = {};
 
-class Form extends React.Component {
+class App extends React.Component {
 	constructor(props) {
 		super(props);
 
 		this.state = {
 			curInterestRate: 0.04,
 			curTotalTaxBurden: getTaxBurden(),
-			person: {
-				debt: {
-					loans: getLoans(),
-					get total () {
-						let amount = 0;
-						const loans = this.loans;
+			debt: {
+				loans: getLoans(),
+				get total () {
+					let amount = 0;
+					const loans = this.loans;
 
-						for (var i = 0; i < loans.length; i++) {
-							amount += loans[i].amount;
-						}
-
-						return amount;
+					for (let i = 0; i < loans.length; i++) {
+						amount += loans[i].amount;
 					}
-				},
-				income: 50000,
-				initialIncome: 50000,
-				get netIncome () {
-					return applyTax(this.income);
-				},
-				nestEgg: 0,
-				savings: 0,
-				yrsRetired: 0,
+
+					return amount;
+				}
 			},
+			income: 50000,
+			initialIncome: 50000,
+			get netIncome () {
+				return applyTax(this.income);
+			},
+			nestEgg: 0,
 			raisePercent: 5,
+			resultData: [],
 			retirementIncome: 200000,
 			savingPercent: 0.15,
+			savings: 10,
 			workingYears: 42,
+			yrsRetired: 0,
 		};
 	}
 
-	handleBlur_1() {
+	handleReset() {
 		let stateObj = this.state;
 		form = this.form;
 
 		this.setState(
 			{
 				curTotalTaxBurden: getTaxBurden(),
+				debt: {
+					loans: getLoans(),
+					get total () {
+						let amount = 0;
+						const loans = this.loans;
+
+						for (let i = 0; i < loans.length; i++) {
+							amount += loans[i].amount;
+						}
+
+						return amount;
+					}
+				},
+				income: parseInt(form.elements['income'].value || 50000),
+				initialIncome: parseInt(form.elements['income'].value || 50000),
+				get netIncome () {
+					return applyTax(this.income);
+				},
+				nestEgg: 0,
 				raisePercent: form.elements['raisePercent'].value ? parseFloat(form.elements['raisePercent'].value) : 5,
+				resultData: [],
 				retirementIncome: form.elements['retIncome'].value ? parseInt(form.elements['retIncome'].value) : 200000,
 				savingPercent: form.elements['savePercent'].value ? parseFloat(form.elements['savePercent'].value / 100) : 0.15,
-				person: {
-					debt: {
-						loans: getLoans(),
-						get total () {
-							let amount = 0;
-							const loans = this.loans;
-
-							for (var i = 0; i < loans.length; i++) {
-								amount += loans[i].amount;
-							}
-
-							return amount;
-						}
-					},
-					income: parseInt(form.elements['income'].value || 50000),
-					initialIncome: parseInt(form.elements['income'].value || 50000),
-					get netIncome () {
-						return applyTax(this.income);
-					},
-					nestEgg: 0,
-					savings: parseInt(form.elements['savings'].value || 0),
-					yrsRetired: 0,
-				},
+				savings: parseInt(form.elements['savings'].value || 0),
 				get workingYears () {
 					let value = stateObj.workingYears;
-
 
 					if (form.elements['retAge'].value && form.elements['curAge'].value) {
 						value = parseInt(form.elements['retAge'].value) - parseInt(form.elements['curAge'].value);
@@ -91,44 +85,129 @@ class Form extends React.Component {
 
 					return value;
 				},
-			}
+				yrsRetired: 0,
+			},
+			this.work
 		);
 	}
 
+	work() {
+		let rows = [];
+		let income = this.state.income;
+		let savings = this.state.savings;
+		let graphData = [];
+
+		for (let year = 1; year <= this.state.workingYears; year++) {
+			let disposibleIncome = this.state.netIncome * this.state.savingPercent;
+
+			if (this.state.debt.total) {
+				disposibleIncome = payOffDebt(disposibleIncome);
+			}
+
+			savings = interest(savings + disposibleIncome);
+
+			income = giveRaise(income, this.state.raisePercent);
+
+			graphData.push({Age: year, 'Net Income': applyTax(income.toFixed(2)), savings: savings.toFixed(2)});
+
+			rows.push(
+				<ResultsData
+					income={applyTax(income).toFixed(2)}
+					key={year}
+					savings={savings.toFixed(2)}
+					year={year}
+				/>
+			);
+		}
+
+		this.setState(
+			{
+				income: income,
+				nestEgg: savings,
+				resultData: rows,
+				savings: savings,
+				graphData: graphData,
+			},
+			this.retire
+		);
+	}
+
+	// finish this function
+	payOffDebt(amount) {
+		// if (this.state.savings) {
+		// 	amount += this.state.savings;
+		// }
+
+		// this.state.debt.loans = [];
+
+		// let leftover = amount - this.state.debt.total;
+
+		// return (leftover > 0) ? leftover : 0;
+		return amount;
+	}
+
+	retire() {
+		let savings = this.state.savings;
+		let yrsRetired = this.state.yrsRetired;
+
+		do {
+			savings = interest(savings, this.state.curInterestRate) - this.state.retirementIncome
+			yrsRetired++
+		} while (savings > this.state.retirementIncome)
+
+		this.setState({
+			savings: savings,
+			yrsRetired: yrsRetired,
+		});
+	}
+
 	calculate() {
-		form = this.form;
-		setVars();
-		makePerson();
-		ReactDOM.render(<Results resultData={work()} />, document.querySelector('.results-wrapper'));
-		retire();
-		ReactDOM.render(<Message person={person} vars={vars} />, document.querySelector('.message-wrapper'));
+		this.work();
+		// this.retire();
 	}
 
 	render () {
 		return (
-			<form action="javascript:;" method="POST" name="retirement" ref={fm => this.form = fm}>
-				<legend>Info</legend>
+			<div className="wrapper">
+				<form action="javascript:;" method="POST" name="retirement" ref={fm => this.form = fm}>
+					<legend>Info</legend>
 
-				<FormField handleBlur={() => this.handleBlur_1()} label="Annual Income (Gross)" name="income" placeholder="$50000" />
-				<FormField handleBlur={() => this.calculate()} label="Current Age" name="curAge" placeholder="25" />
-				<FormField handleBlur={() => this.calculate()} label="Target Retirement Age" name="retAge" placeholder="67" />
-				<FormField handleBlur={() => this.calculate()} label="Current Savings" name="savings" placeholder="$0" />
-				<FormField handleBlur={() => this.calculate()} label="Percent of Net income saved" name="savePercent" placeholder="15%" />
-				<FormField handleBlur={() => this.calculate()} label="Expected average raise" name="raisePercent" placeholder="5%" />
-				<FormField handleBlur={() => this.calculate()} label="Target Retirement Income" name="retIncome" placeholder="$200000" />
+					<FormField handleBlur={() => this.handleReset()} label="Annual Income (Gross)" name="income" placeholder="$50000" />
+					<FormField handleBlur={() => this.handleReset()} label="Current Age" name="curAge" placeholder="25" />
+					<FormField handleBlur={() => this.handleReset()} label="Target Retirement Age" name="retAge" placeholder="67" />
+					<FormField handleBlur={() => this.handleReset()} label="Current Savings" name="savings" placeholder="$0" />
+					<FormField handleBlur={() => this.handleReset()} label="Percent of Net income saved" name="savePercent" placeholder="15%" />
+					<FormField handleBlur={() => this.handleReset()} label="Expected average raise" name="raisePercent" placeholder="5%" />
+					<FormField handleBlur={() => this.handleReset()} label="Target Retirement Income" name="retIncome" placeholder="$200000" />
 
-				<label>Debt</label>
-				<Debt />
+					<label>Debt</label>
+					<Debt />
 
-				<button type="submit" className="btn btn-primary" onClick={() => this.calculate()}>Calculate</button>
-			</form>
+					<button type="submit" className="btn btn-primary" onClick={() => this.handleReset()}>Calculate</button>
+				</form>
+
+				<Results resultData={this.state.resultData} />
+
+				<Message
+					nestEgg={this.state.nestEgg}
+					yrsRetired={this.state.yrsRetired}
+					retirementIncome={this.state.retirementIncome}
+					workingYears={this.state.workingYears}
+					initialIncome={this.state.initialIncome}
+					raisePercent={this.state.raisePercent}
+					savingPercent={this.state.savingPercent}
+					curInterestRate={this.state.curInterestRate}
+					yrsRetired={this.state.yrsRetired}
+					workingYears={this.state.workingYears}
+				/>
+			</div>
 		);
 	}
 }
 
 ReactDOM.render(
-	<Form />,
-	document.querySelector('.form-wrapper')
+	<App />,
+	document.querySelector('.app')
 );
 
 function applyTax(amount, rate) {
@@ -151,7 +230,7 @@ function getLoans() {
 
 		loanNodes.forEach(
 			item => {
-				var loan = {
+				let loan = {
 					amount: parseInt(item.querySelector('.debt').value),
 					interestRate: parseInt(item.querySelector('.interest').value)
 				};
@@ -184,99 +263,11 @@ function giveRaise(salery, percent) {
 }
 
 function interest(amount, rate) {
-	rate = rate || vars.curInterestRate;
+	rate = rate || 0;
 
 	if (rate < 1) {
 		rate += 1;
 	}
 
 	return amount * rate;
-}
-
-function makePerson() {
-	person = {
-		debt: {
-			loans: getLoans(),
-			get total () {
-				let amount = 0;
-				const loans = this.loans;
-
-				for (var i = 0; i < loans.length; i++) {
-					amount += loans[i].amount;
-				}
-
-				return amount;
-			}
-		},
-		income: parseInt(form.elements['income'].value || 50000),
-		initialIncome: parseInt(form.elements['income'].value || 50000),
-		get netIncome () {
-			return applyTax(this.income);
-		},
-		nestEgg: 0,
-		savings: parseInt(form.elements['savings'].value || 0),
-		workingYears: vars.workingYears,
-		yrsRetired: 0
-	};
-}
-
-// finish this function
-function payOffDebt(amount) {
-	if (person.savings) {
-		amount += person.savings;
-	}
-
-	person.debt.loans = [];
-
-	let leftover = amount - person.debt.total;
-
-	return (leftover > 0) ? leftover : 0;
-}
-
-function retire() {
-	do {
-		person.savings = interest(person.savings);
-		person.savings = person.savings - vars.retirementIncome;
-
-		person.yrsRetired++;
-	} while (person.savings > vars.retirementIncome)
-}
-
-function setVars() {
-	vars.curInterestRate = 0.04;
-	vars.curTotalTaxBurden = getTaxBurden();
-	vars.raisePercent = form.elements['raisePercent'].value ? parseFloat(form.elements['raisePercent'].value) : 5;
-	vars.retirementIncome = form.elements['retIncome'].value ? parseInt(form.elements['retIncome'].value) : 200000;
-	vars.savingPercent = form.elements['savePercent'].value ? parseFloat(form.elements['savePercent'].value / 100) : 0.15;
-	vars.workingYears = 42;
-
-	if (form.elements['retAge'].value && form.elements['curAge'].value) {
-		vars.workingYears = parseInt(form.elements['retAge'].value) - parseInt(form.elements['curAge'].value);
-	}
-}
-
-function work() {
-	var rows = [];
-	for (let year = 1; year <= vars.workingYears; year++) {
-		var disposibleIncome = person.netIncome * vars.savingPercent;
-
-		if (person.debt.total) {
-			disposibleIncome = payOffDebt(disposibleIncome);
-		}
-
-		person.savings = interest(person.savings + disposibleIncome);
-
-		person.income = giveRaise(person.income, vars.raisePercent);
-
-		rows.push(
-			<ResultsData
-				income={person.netIncome.toFixed(2)}
-				key={year}
-				savings={person.savings.toFixed(2)}
-				year={year}
-			/>);
-	}
-	person.nestEgg = parseInt(person.savings);
-
-	return rows;
 }
